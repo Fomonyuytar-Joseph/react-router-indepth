0cce4d910fea8c3941f87baffb4482db8eaa156f
Fomonyuytar-Joseph <josephneymar33@gmail.com>
2023-08-15 08:49:29 +0100

    params

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import About from './pages/About';
 import Contact from './pages/help/Contact';
 import Faq from './pages/help/Faq';
 import NotFound from './pages/NotFound';
+import Careers, { careersLoader } from './pages/careers/Careers';
+import CareerDetails, { careerDetailsLoader } from "./pages/careers/CareerDetails";
+
 
 import RootLayout from './layouts/RootLayout';
 import HelpLayout from './layouts/HelpLayout';
+import CareersLayout from './layouts/CareersLayout';
 
 
 const router = createBrowserRouter(
@@ -14,10 +18,29 @@ const router = createBrowserRouter(
     <Route path="/" element={<RootLayout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
+
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq/>} />
         <Route path="contact" element={<Contact/>}/>
       </Route>
+
+      <Route path="careers" element={<CareersLayout />}>
+        <Route 
+        index 
+        element={<Careers/>} 
+        loader={careersLoader}
+        />
+
+        <Route
+        path=':id'
+        element={<CareerDetails/>}
+        loader={careerDetailsLoader}
+        />
+        
+      </Route>
+
+
+      
 
       <Route path='*' element={<NotFound/>}/>
     </Route>
